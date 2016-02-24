c31e27d8b17d9abb87a6e04bea9180d808fd878c
Eamonn de Leastar <edeleastar@wit.ie>
2016-02-24 11:52:04 +0000

    added ActivitiesList activity

---
+++ app/src/main/java/org/pacemaker/pacemaker/ActivitiesList.java
@@ -0,0 +1,15 @@
+package org.pacemaker.pacemaker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivitiesList extends AppCompatActivity
+{
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState)
+  {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_activities_list);
+  }
+}
