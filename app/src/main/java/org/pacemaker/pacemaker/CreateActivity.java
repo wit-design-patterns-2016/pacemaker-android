80f6eff1f46e8cea7f034a4c28850cbc1972f659
Eamonn de Leastar <edeleastar@wit.ie>
2016-02-24 07:30:40 +0000

    introduce button widget + event handler

--- app/src/main/java/org/pacemaker/pacemaker/CreateActivity.java
+++ app/src/main/java/org/pacemaker/pacemaker/CreateActivity.java
@@ -2,12 +2,27 @@ package org.pacemaker.pacemaker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class CreateActivity extends AppCompatActivity {
+public class CreateActivity extends AppCompatActivity
+{
+  private Button createActivityButton;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create);
-    }
+  @Override
+  protected void onCreate(Bundle savedInstanceState)
+  {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_create);
+
+    createActivityButton = (Button) findViewById(R.id.createActivityButton);
+
+    Log.v("Pacemaker", "got the CreateActivity button");
+  }
+
+  public void createActivityButtonPressed (View view)
+  {
+    Log.v("Pacemaker", "CreateActivity Button Pressed!");
+  }
 }
