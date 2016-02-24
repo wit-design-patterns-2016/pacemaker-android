68d41016241213bcfd9c923d48f653b3db6129c7
Eamonn de Leastar <edeleastar@wit.ie>
2016-02-24 07:55:52 +0000

    wired up controls in CreateActivity class

--- app/src/main/java/org/pacemaker/pacemaker/CreateActivity.java
+++ app/src/main/java/org/pacemaker/pacemaker/CreateActivity.java
@@ -5,10 +5,16 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.NumberPicker;
+import android.widget.TextView;
+
 
 public class CreateActivity extends AppCompatActivity
 {
-  private Button createActivityButton;
+  private Button       createActivityButton;
+  private TextView     activityType;
+  private TextView     activityLocation;
+  private NumberPicker distancePicker;
 
   @Override
   protected void onCreate(Bundle savedInstanceState)
@@ -16,13 +22,20 @@ public class CreateActivity extends AppCompatActivity
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_create);
 
-    createActivityButton = (Button) findViewById(R.id.createActivityButton);
+    createActivityButton = (Button)       findViewById(R.id.createActivityButton);
+    activityType         = (TextView)     findViewById(R.id.activityType);
+    activityLocation     = (TextView)     findViewById(R.id.activityLocation);
+    distancePicker       = (NumberPicker) findViewById(R.id.numberPicker);
+
+    distancePicker.setMinValue(0);
+    distancePicker.setMaxValue(20);
 
     Log.v("Pacemaker", "got the CreateActivity button");
   }
 
   public void createActivityButtonPressed (View view)
   {
-    Log.v("Pacemaker", "CreateActivity Button Pressed!");
+    int distance = distancePicker.getValue();
+    Log.v("Pacemaker", "CreateActivity Button Pressed with " + distance);
   }
 }
