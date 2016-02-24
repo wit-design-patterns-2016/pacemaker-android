506ae594d722c030d6d65c606133b11671a4abbb
Eamonn de Leastar <edeleastar@wit.ie>
2016-02-24 13:05:01 +0000

    replace stock adapter with a customised ArrayAdapter

--- app/src/main/java/org/pacemaker/pacemaker/ActivitiesList.java
+++ app/src/main/java/org/pacemaker/pacemaker/ActivitiesList.java
@@ -1,10 +1,15 @@
 package org.pacemaker.pacemaker;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -25,8 +30,41 @@ public class ActivitiesList extends AppCompatActivity
 
     List<MyActivity> activities  = app.actvities;
 
-    ArrayAdapter <MyActivity>activitiesAdapter = new ArrayAdapter<MyActivity>(this, android.R.layout.simple_list_item_1, activities);
+    ActivityAdapter activitiesAdapter = new ActivityAdapter(this,  activities);
     activitiesListView.setAdapter(activitiesAdapter);
     activitiesAdapter.notifyDataSetChanged();
+  }
+}
+
+class ActivityAdapter extends ArrayAdapter<MyActivity>
+{
+  private Context        context;
+  public  List<MyActivity> activities;
+
+  public ActivityAdapter(Context context, List<MyActivity> activities)
+  {
+    super(context, android.R.layout.simple_list_item_1, activities);
+    this.context   = context;
+    this.activities = activities;
+  }
+
+  @Override
+  public View getView(int position, View convertView, ViewGroup parent)
+  {
+    LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+    View     view     = inflater.inflate(android.R.layout.simple_list_item_1, parent, false);
+    MyActivity activity = activities.get(position);
+    TextView textView = (TextView) view.findViewById(android.R.id.text1);
+
+    textView.setText("" + activity);
+
+    return view;
+  }
+
+  @Override
+  public int getCount()
+  {
+    return activities.size();
   }
 }
