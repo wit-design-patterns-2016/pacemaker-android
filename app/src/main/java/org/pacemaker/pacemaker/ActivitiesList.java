4821fbf207c2a6a307ad1172c3406ce05023885a
Eamonn de Leastar <edeleastar@wit.ie>
2016-02-24 12:52:03 +0000

    render the activities with a simple ArrayAdapter

--- app/src/main/java/org/pacemaker/pacemaker/ActivitiesList.java
+++ app/src/main/java/org/pacemaker/pacemaker/ActivitiesList.java
@@ -3,11 +3,14 @@ package org.pacemaker.pacemaker;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.util.List;
 
 public class ActivitiesList extends AppCompatActivity
 {
+  private ListView activitiesListView;
 
   @Override
   protected void onCreate(Bundle savedInstanceState)
@@ -18,9 +21,15 @@ public class ActivitiesList extends AppCompatActivity
     Bundle extras = getIntent().getExtras();
     List<MyActivity> activities  = extras.getParcelableArrayList("activities");
 
+    activitiesListView = (ListView) findViewById(R.id.activitiesListView);
+
     for (MyActivity activity : activities)
     {
       Log.v("Pacemaker", "Activity: " + activity);
     }
+
+    ArrayAdapter<MyActivity> activitiesAdapter = new ArrayAdapter<MyActivity>(this, android.R.layout.simple_list_item_1, activities);
+    activitiesListView.setAdapter(activitiesAdapter);
+    activitiesAdapter.notifyDataSetChanged();
   }
 }
