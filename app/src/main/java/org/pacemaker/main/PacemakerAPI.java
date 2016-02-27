2d9c381bb0c8ad38c91a6bb36c1c49eaa3fd8b8a
Eamonn de Leastar <edeleastar@wit.ie>
2016-02-27 09:23:12 +0000

    extend API to retrieve/upload activities

--- app/src/main/java/org/pacemaker/main/PacemakerAPI.java
+++ app/src/main/java/org/pacemaker/main/PacemakerAPI.java
@@ -5,6 +5,7 @@ import org.pacemaker.http.Request;
 import org.pacemaker.http.Response;
 import org.pacemaker.http.Rest;
 import org.pacemaker.models.JsonParser;
+import org.pacemaker.models.MyActivity;
 import org.pacemaker.models.User;
 import android.content.Context;
 
@@ -18,6 +19,16 @@ public class PacemakerAPI
   public static void createUser(Context context, Response<User> response, String dialogMesssage, User user)
   {
     new CreateUser(context, response, dialogMesssage).execute(user);
+  }
+
+  public static void getActivities(Context context, User user, Response<MyActivity> response, String dialogMesssage)
+  {
+    new GetActivities(context, user, response, dialogMesssage).execute();
+  }
+
+  public static void createActivity(Context context, User user, Response<MyActivity> response, String dialogMesssage, MyActivity activity)
+  {
+    new CreateActivity(context, user, response, dialogMesssage).execute(activity);
   }
 }
 
@@ -49,5 +60,42 @@ class CreateUser extends Request
   {
     String response = Rest.post("/api/users", JsonParser.user2Json(params[0]));
     return JsonParser.json2User(response);
+  }
+}
+
+class GetActivities extends Request
+{
+  private User user;
+
+  public GetActivities(Context context, User user, Response<MyActivity> callback, String message)
+  {
+    super(context, callback, message);
+    this.user = user;
+  }
+
+  @Override
+  protected List<MyActivity> doRequest(Object... params) throws Exception
+  {
+    String response =  Rest.get("/api/users/" + user.id + "/activities");
+    List<MyActivity> ActivityList = JsonParser.json2Activities(response);
+    return ActivityList;
+  }
+}
+
+class CreateActivity extends Request
+{
+  private User user;
+
+  public CreateActivity(Context context, User user, Response<MyActivity> callback, String message)
+  {
+    super(context, callback, message);
+    this.user = user;
+  }
+
+  @Override
+  protected MyActivity doRequest(Object... params) throws Exception
+  {
+    String response = Rest.post ("/api/users/" + user.id + "/activities", JsonParser.activity2Json(params[0]));
+    return JsonParser.json2Activity(response);
   }
 }
