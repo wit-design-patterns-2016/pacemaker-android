f594c78c38deb0d1c80e59b10e5e17b58495cd3f
Eamonn de Leastar <edeleastar@wit.ie>
2016-02-27 08:56:49 +0000

    in Welcome, download list of users from pacemaker-play service

--- app/src/main/java/org/pacemaker/controllers/Welcome.java
+++ app/src/main/java/org/pacemaker/controllers/Welcome.java
@@ -2,6 +2,7 @@ package org.pacemaker.controllers;
 
 import org.pacemaker.R;
 import org.pacemaker.main.PacemakerApp;
+import org.pacemaker.main.PacemakerAPI;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -11,21 +12,22 @@ import android.view.View;
 public class Welcome extends Activity
 {
   PacemakerApp app;
-  
+
   @Override
   public void onCreate(Bundle savedInstanceState)
   {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_welcome);
     app = (PacemakerApp) getApplication();
+    PacemakerAPI.getUsers(this, app, "Retrieving list of users");
   }
 
-  public void loginPressed (View view) 
+  public void loginPressed (View view)
   {
     startActivity (new Intent(this, Login.class));
   }
-  
-  public void signupPressed (View view) 
+
+  public void signupPressed (View view)
   {
     startActivity (new Intent(this, Signup.class));
   }
